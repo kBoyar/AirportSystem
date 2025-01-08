c619f4e52888ffd2147d9b9f88602ec227da9936
kboyar <tagasbg@gmail.com>
2025-01-09 00:35:09 +0200

    ViewModel created

---
+++ ViewModel/viewmodel.py
@@ -0,0 +1,15 @@
+class ViewModel:
+    def __init__(self,model_manager):
+        self.model_manager = model_manager
+
+    def load_employees(self):
+        employee_model = self.model_manager.get_model("employees")
+        return employee_model.get_employees()
+
+    def add_employee(self, first_name, last_name, position, password):
+        employee_model = self.model_manager.get_model("employees")
+        employee_model.add_employee(first_name,last_name,position,password)
+
+    def edit_employee(self, selected, prop, value):
+        employee_model = self.model_manager.get_model("employees")
+        employee_model.edit_employee(selected, prop, value)
